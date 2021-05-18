8a7ca493077308d8b08a5b1ed1878882475445b8
Lisanne Ardern <ardern.lisanne@gmail.com>
2021-05-18 12:15:40 +0200

    Add components to App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import Button from './Button'
+import Player from './Player'
+import PlayerForm from './PlayerForm'
 
 function App() {
+  const [players, setPlayers] = useState([
+    { name: 'Jane Doe', score: 20 },
+    { name: 'John Doe', score: 50 },
+  ])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {players.map((player, index) => (
+        <Player
+          key={player.name}
+          onMinus={() => handleMinus(index)}
+          onPlus={() => handlePlus(index)}
+          name={player.name}
+          score={player.score}
+        />
+      ))}
+      <Button onClick={resetScores}>Reset scores</Button>
+      <Button onClick={resetAll}>Reset All</Button>
+      <PlayerForm onSubmit={createPlayer} />
     </div>
-  );
+  )
+
+  function handleMinus(index) {
+    const playerToUpdate = players[index]
+    setPlayers([
+      ...players.slice(0, index),
+      { ...playerToUpdate, score: playerToUpdate.score - 1 },
+      ...players.slice(index + 1),
+    ])
+  }
+
+  function handlePlus(index) {
+    const playerToUpdate = players[index]
+    setPlayers([
+      ...players.slice(0, index),
+      { ...playerToUpdate, score: playerToUpdate.score + 1 },
+      ...players.slice(index + 1),
+    ])
+  }
+
+  function resetScores() {
+    setPlayers(players.map(player => ({ ...player, score: 0 })))
+  }
+
+  function resetAll() {
+    setPlayers([])
+  }
+
+  function createPlayer(name) {
+    setPlayers([...players, { name, score: 0 }])
+  }
 }
 
-export default App;
+export default App
