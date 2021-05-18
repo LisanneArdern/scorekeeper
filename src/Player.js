7fbe861f64bde3cf3e495611cfcc767907c9c9de
Lisanne Ardern <ardern.lisanne@gmail.com>
2021-05-18 20:18:59 +0200

    Add component testing

--- src/Player.js
+++ src/Player.js
@@ -12,9 +12,9 @@ export default function Player({ name, score, onMinus, onPlus }) {
   return (
     <section className="Player">
       <div>{name}</div>
-      <button onClick={onMinus}>&minus;</button>
+      <button onClick={onMinus}>-</button>
       <div className="Player__score">{score}</div>
-      <button onClick={onPlus}>&#43;</button>
+      <button onClick={onPlus}>+</button>
     </section>
   )
 }
