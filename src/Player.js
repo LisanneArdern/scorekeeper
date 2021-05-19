0058a3652ce2168bcce622568c648a5b658ec532
Lisanne Ardern <ardern.lisanne@gmail.com>
2021-05-19 10:52:04 +0200

    Add styled components to Player.js

--- src/Player.js
+++ src/Player.js
@@ -1,5 +1,5 @@
-import './Player.css'
 import PropTypes from 'prop-types'
+import styled from 'styled-components/macro'
 
 Player.propTypes = {
   name: PropTypes.string.isRequired,
@@ -10,11 +10,36 @@ Player.propTypes = {
 
 export default function Player({ name, score, onMinus, onPlus }) {
   return (
-    <section className="Player">
+    <Wrapper>
       <div>{name}</div>
       <button onClick={onMinus}>-</button>
-      <div className="Player__score">{score}</div>
+      <Score>{score}</Score>
       <button onClick={onPlus}>+</button>
-    </section>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.section`
+  display: grid;
+  grid-template-columns: 4fr 1fr 1fr 1fr;
+  align-items: center;
+  gap: 20px;
+
+  button {
+    padding: 0.35em 1.2em;
+    border: 0.1em solid black;
+    margin: 0 0.3em 0.3em 0;
+    display: inline-block;
+    border-radius: 0.12em;
+    box-sizing: border-box;
+    text-decoration: none;
+    font-family: 'Roboto', sans-serif;
+    font-weight: 300;
+    color: black;
+    text-align: center;
+    background-color: white;
+  }
+`
+const Score = styled.div`
+  justify-self: center;
+`
