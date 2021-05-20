cd243ca78d8b922f37254b4b8c0b40086523f2a3
Lisanne Ardern <ardern.lisanne@gmail.com>
2021-05-20 14:49:07 +0200

    Prepare for app version b

--- src/components/Navigation/Navigation.stories.js
+++ src/components/Navigation/Navigation.stories.js
@@ -1,5 +1,4 @@
 import React from 'react'
-
 import Navigation from './Navigation'
 
 export default {
@@ -9,8 +8,12 @@ export default {
 
 const Template = args => <Navigation {...args} />
 
-export const DefaultNavigation = Template.bind({})
-DefaultNavigation.args = {}
+export const Default = Template.bind({})
 
-export const ActiveNavigation = Template.bind({})
-ActiveNavigation.args = {}
+Default.args = {
+  currentPageId: 'play',
+  pages: [
+    { title: 'Play', id: 'play' },
+    { title: 'History', id: 'history' },
+  ],
+}
